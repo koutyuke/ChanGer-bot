3fd6f4a4a4e54268f01336641cfefff0db771995
st20063ks <103472642+st20063ks@users.noreply.github.com>
2022-04-30 16:29:28 +0900

    add/module

---
+++ module/discordBotFunc.py
@@ -0,0 +1,24 @@
+import discord
+from requests import post, request
+from dotenv import load_dotenv
+from flask import Flask,jsonify,request as FlaskRequest
+from discord import Guild,Member
+
+
+def channel_create():
+  
+  
+  return
+
+
+def channel_delete():
+  
+  
+  
+  return
+
+
+def voice_state():
+  
+  
+  return
