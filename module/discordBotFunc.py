930ff39b20a31e9d39aa615cf0a8fb89140a714a
st20063ks <103472642+st20063ks@users.noreply.github.com>
2022-05-01 12:29:04 +0900

    change/discordBotFunc 🔨

--- module/discordBotFunc.py
+++ module/discordBotFunc.py
@@ -1,9 +1,4 @@
-import discord
-from requests import post, request
-from dotenv import load_dotenv
-from flask import Flask,jsonify,request as FlaskRequest
 from discord import Guild,Member,VoiceChannel,VoiceState
-
 
 def channel_create(channel:VoiceChannel) -> dict:
   guild:Guild = channel.guild
@@ -35,11 +30,10 @@ def channel_delete(channel:VoiceChannel):
 
 
 def voice_state(before:VoiceState,after:VoiceState,user:Member):
-  
   beforeChannel = before.channel
   afterChannel = after.channel
+  
   voiceType = "connect" if beforeChannel is None else ("disconnect" if afterChannel is None else "change")
-
   beforeId = None if voiceType == "connect" else before.channel.id
   afterId = None if voiceType == "disconnect" else after.channel.id
   
